1d8486ecc55934c9be1e86c755abc8998b8ee1c0
hoang minh <hoangrbs2@gmail.com>
2021-01-29 08:42:52 +0700

    add config

---
+++ database_config.cpp
@@ -0,0 +1,20 @@
+// 1. download xampp server: a development server that already contains mysql server and its admin to 
+// watch the database
+// link tutorial: https://www.youtube.com/watch?v=3B-CnezwEeo&list=PL4cUxeGkcC9gksOX3Kd9KPo-O68ncT05o&index=2
+
+// 2. we need a library for C++ to communicate with mysql (interface)
+// -> download MySQL Community Server: 
+// choose Windows (x86, 64-bit), ZIP Archive here: https://dev.mysql.com/downloads/mysql/
+// -> extract
+
+// ---------- do this if only you did not config --------------
+// 3. config project properies with the extracted folder so visual studio project can
+// link and connect to the mysql library, the config tutorial is in the very last link below
+
+// if you get error : "Adding an include path crashes VS2019 100% of the time":
+// https://developercommunity.visualstudio.com/content/problem/677979/adding-an-include-path-crashes-vs2019-100-of-the-t.html
+// then just update visual studio: https://docs.microsoft.com/en-us/visualstudio/install/update-visual-studio?view=vs-2019
+// ------------------------------------------------------------
+
+// link tutorial that combine all steps above: https://www.youtube.com/watch?v=-7u1lgRzhpI
+
