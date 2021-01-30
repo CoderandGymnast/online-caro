458863bf57ff85c9feede789adc14dd26d7b698d
hoang minh <hoangrbs2@gmail.com>
2021-01-31 01:41:11 +0700

    file sql to import

--- dependencies/caro_winsock_server.sql
+++ dependencies/caro_winsock_server.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 29, 2021 at 04:19 PM
+-- Generation Time: Jan 30, 2021 at 07:40 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.33
 
@@ -48,11 +48,35 @@ INSERT INTO `test_table` (`id`, `username`) VALUES
 --
 
 CREATE TABLE `user_list` (
-  `id` int(2) NOT NULL,
+  `id` int(5) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
-  `score` int(11) NOT NULL
+  `score` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_list`
+--
+
+INSERT INTO `user_list` (`id`, `username`, `password`, `score`) VALUES
+(1, 'hoang1', '123456', 13),
+(2, 'hoang2', '123456', 0),
+(12, 'hoang4', '123456', 0),
+(29, 'alice', '123456', 25),
+(31, 'alice2', '123456', 30),
+(32, 'alice3', '123456', 27),
+(33, 'alice4', '123456', 15),
+(34, 'alice5', '123456', 10),
+(35, 'alice6', '123456', 6),
+(36, 'alice7', '123456', 8),
+(37, 'alice8', '123456', 25),
+(38, 'alice9', '123456', 25),
+(39, 'alice10', '123456', 27),
+(40, 'alierger', '123456', 25),
+(41, 'alerger', '123456', 20),
+(42, 'etgr5e', '123456', 17),
+(43, 'tgrtg', '123456', 16),
+(44, 'tergerg', '123456', 14);
 
 --
 -- Indexes for dumped tables
@@ -86,7 +110,7 @@ ALTER TABLE `test_table`
 -- AUTO_INCREMENT for table `user_list`
 --
 ALTER TABLE `user_list`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
